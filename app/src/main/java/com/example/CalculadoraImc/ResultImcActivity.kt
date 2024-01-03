e2ae61bfe78e949d3092ddcb7edbc5d98f0de05b
mateuscosta27 <mateuscosta27@gmail.com>
2024-01-03 17:25:18 -0400

    Adicionando botão de recalcular e fechar o app, adicionando icone ao app

--- app/src/main/java/com/example/CalculadoraImc/ResultImcActivity.kt
+++ app/src/main/java/com/example/CalculadoraImc/ResultImcActivity.kt
@@ -14,6 +14,12 @@ class ResultImcActivity : AppCompatActivity() {
         binding = ActivityResultImcBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getExtra()
+        binding.buttonRecalculate.setOnClickListener {
+            returnForRecalculate()
+        }
+        binding.buttonFinishApp.setOnClickListener {
+            finishAffinity()
+        }
     }
 
     private fun getExtra() {
@@ -26,27 +32,58 @@ class ResultImcActivity : AppCompatActivity() {
     }
 
     private fun SetText(imc: Double) {
-        val categoryImc = binding.textCategoryImc
+        val textDescriptionCategory = binding.textCategoryImc
+        val textResultDescription = binding.textResultDescription
+
 
         when {
             imc < 18.5 -> {
-                categoryImc.text = getString(R.string.underWeightCategory)
-                categoryImc.setTextColor(getColor(R.color.colorUnderWeight))
+                textDescriptionCategory.text = getString(R.string.underWeightCategory)
+                textResultDescription.text = getString(R.string.underWeight)
+
             }
 
             imc in 18.51..24.9 -> {
-                categoryImc.text = getString(R.string.idealWeight)
+                textDescriptionCategory.text = getString(R.string.idealWeightCategory)
+                textResultDescription.text = getString(R.string.idealWeight)
+
 
             }
 
-            imc in 25.0..29.9 -> println("SobrePeso")
-            imc in 30.0..34.9 -> println("Obesidade Grau I")
-            imc in 35.0..39.9 -> println("Obesidade Grau II")
-            imc > 40 -> "Obresidade Grau III"
+            imc in 25.0..29.9 -> {
+                textDescriptionCategory.text = getString(R.string.overWeightCategory)
+                textResultDescription.text = getString(R.string.overweight)
+
+            }
+
+            imc in 30.0..34.9 -> {
+                textDescriptionCategory.text = getString(R.string.firstDegreeObesityCaterogy)
+                textResultDescription.text = getString(R.string.firstDegreeObesityCaterogy)
+
+            }
+
+            imc in 35.0..39.9 -> {
+                textDescriptionCategory.text = getString(R.string.secondDegreeObesityCategory)
+                textResultDescription.text = getString(R.string.secondDegreeObesity)
+
+            }
+
+            imc > 40 -> {
+                textDescriptionCategory.text = getString(R.string.thirdDegreeObesityCategory)
+                textResultDescription.text = getString(R.string.thirdDegreeObesity)
+            }
+
             else -> println("fora da faixa")
 
 
         }
+
+    }
+
+    private fun returnForRecalculate() {
+        intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
 
     }
 
